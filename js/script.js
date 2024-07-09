bb2fac7c6f6a90b72cc8154f421a15368fcd9b67
Владислав <arshavin0309@mail.ru>
2024-07-09 12:26:34 +0500

    add main without mobile

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // появление хедера при скролле
+    let body = document.querySelector('body');
+    let header = document.querySelector('header');
+
+    window.onscroll = function () {
+        if (pageYOffset > 50) {
+            header.classList.add('fixed');
+            body.style = 'margin-top: 100px'
+        } else {
+            header.classList.remove('fixed');
+            body.style = 'margin: 0'
+        }
+    }
+
     document.querySelector('.burger').addEventListener('click', () => {
         document.querySelector('.header__nav').classList.toggle('show')
     })
@@ -34,4 +48,23 @@ document.addEventListener('DOMContentLoaded', () => {
             el: '.swiper-scrollbar',
         },
     });
+
+    // табы
+    let buttons = document.querySelectorAll('.analyst__buttons button')
+    let tables = document.querySelectorAll('.analyst__widget')
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', () => {
+            for (let n = 0; n < buttons.length; n++) {
+                buttons[n].classList.remove('active')
+            }
+
+            buttons[i].classList.add('active')
+
+            for (let el = 0; el < tables.length; el++) {
+                tables[el].classList.remove('active')
+                tables[i].classList.add('active')
+            }
+        })
+    }
 })
